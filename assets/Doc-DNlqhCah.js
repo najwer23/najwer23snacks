e9bc109396210177e4a8b2e91916813d760146d4
Mariusz Najwer <najwer23@gmail.com>
2024-08-23 08:17:23 +0200

    Updates

---
+++ assets/Doc-DNlqhCah.js
@@ -0,0 +1,8 @@
+import{j as e}from"./jsx-runtime-DEdD30eg.js";import{useMDXComponents as o}from"./index-CcnH5Kt0.js";import{ae as s}from"./index-gdZtrxad.js";import"./index-RYns6xqu.js";import"./iframe-eqJF3_1_.js";import"../sb-preview/runtime.js";import"./index-D16Yfzz8.js";import"./index-D-8MO0q_.js";import"./index-B23dhaOI.js";import"./index-DrFu-skq.js";function t(r){const n={a:"a",h1:"h1",li:"li",p:"p",ul:"ul",...o(),...r.components};return e.jsxs(e.Fragment,{children:[e.jsx(s,{title:"Doc"}),`
+`,e.jsx(n.h1,{id:"turborepo-design-system-for-reactjs-with-storybook",children:"Turborepo Design System for React.js with Storybook"}),`
+`,e.jsx(n.p,{children:"Mariusz Najwer"}),`
+`,e.jsx("div",{style:{display:"flex",gap:"20px",alignItems:"center",margin:"20px 0 20px"},children:e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:e.jsx(n.a,{href:"https://najwer23.github.io/najwer23snacks",rel:"nofollow",children:"Web"})}),`
+`,e.jsx(n.li,{children:e.jsx(n.a,{href:"https://github.com/najwer23/najwer23snacks",rel:"nofollow",children:"Github"})}),`
+`,e.jsx(n.li,{children:e.jsx(n.a,{href:"https://www.npmjs.com/search?q=najwer23snacks",rel:"nofollow",children:"NPM"})}),`
+`]})})]})}function u(r={}){const{wrapper:n}={...o(),...r.components};return n?e.jsx(n,{...r,children:e.jsx(t,{...r})}):t(r)}export{u as default};
