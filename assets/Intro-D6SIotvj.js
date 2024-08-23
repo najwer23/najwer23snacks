1a1054d70ddd7b09570c814a062df282c91dad94
Mariusz Najwer <najwer23@gmail.com>
2024-08-23 07:08:46 +0200

    Updates

---
+++ assets/Intro-D6SIotvj.js
@@ -0,0 +1,6 @@
+import{j as t}from"./jsx-runtime-DEdD30eg.js";import{useMDXComponents as o}from"./index-CcnH5Kt0.js";import{ae as i}from"./index-CaFA9edT.js";import"./index-RYns6xqu.js";import"./iframe-DcHoCpdl.js";import"../sb-preview/runtime.js";import"./index-D16Yfzz8.js";import"./index-D-8MO0q_.js";import"./index-B23dhaOI.js";import"./index-DrFu-skq.js";function n(e){const r={a:"a",h1:"h1",li:"li",p:"p",ul:"ul",...o(),...e.components};return t.jsxs(t.Fragment,{children:[t.jsx(i,{title:"Intro"}),`
+`,t.jsx(r.h1,{id:"intro---turborepo-design-system-for-reactjs-with-storybook",children:"Intro - Turborepo Design System for React.js with Storybook"}),`
+`,t.jsx(r.p,{children:"Mariusz Najwer"}),`
+`,t.jsx("div",{style:{display:"flex",gap:"20px",alignItems:"center",margin:"20px 0 20px"},children:t.jsxs(r.ul,{children:[`
+`,t.jsxs(r.li,{children:[t.jsx(r.a,{href:"https://github.com/najwer23/snacks",rel:"nofollow",children:"Github"})," — JavaScript/TypeScript library for user interfaces"]}),`
+`]})})]})}function d(e={}){const{wrapper:r}={...o(),...e.components};return r?t.jsx(r,{...e,children:t.jsx(n,{...e})}):n(e)}export{d as default};
