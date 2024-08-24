cb7e2cf1e06b0cdd54cdece034a8154c2b11857d
Mariusz Najwer <najwer23@gmail.com>
2024-08-24 13:51:54 +0200

    Updates

---
+++ assets/Doc-BXL5jll3.js
@@ -0,0 +1,44 @@
+import{j as e}from"./jsx-runtime-DEdD30eg.js";import{useMDXComponents as r}from"./index-CcnH5Kt0.js";import{ae as t}from"./index-D0PW1jhm.js";import"./index-RYns6xqu.js";import"./iframe-2-81gfQ7.js";import"../sb-preview/runtime.js";import"./index-D16Yfzz8.js";import"./index-D-8MO0q_.js";import"./index-B23dhaOI.js";import"./index-DrFu-skq.js";function i(s){const n={a:"a",h1:"h1",h2:"h2",h3:"h3",img:"img",li:"li",ol:"ol",p:"p",ul:"ul",...r(),...s.components};return e.jsxs(e.Fragment,{children:[e.jsx(t,{title:"Doc"}),`
+`,e.jsx(n.h1,{id:"najwer23snacks---design-system",children:"@najwer23snacks - Design System"}),`
+`,e.jsx(n.h3,{id:"a-bundle-of-react-components-to-use-in-your-application",children:"A bundle of React components to use in your application!"}),`
+`,e.jsx(n.p,{children:'A design system is a comprehensive collection of reusable components, guidelines, and standards that help teams create consistent and cohesive digital products. It serves as a "single source of truth," ensuring that design and development processes are streamlined across various platforms and teams.'}),`
+`,e.jsxs(n.ol,{children:[`
+`,e.jsx(n.li,{children:"Reusable UI Components"}),`
+`,e.jsx(n.li,{children:"Documentation and Guidelines"}),`
+`,e.jsx(n.li,{children:"Style Guides"}),`
+`,e.jsx(n.li,{children:"Design Tokens"}),`
+`]}),`
+`,e.jsx(n.h1,{id:"benefits-of-using-a-design-system",children:"Benefits of Using a Design System"}),`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsxs(n.li,{children:[`
+`,e.jsx(n.p,{children:"Increased Efficiency: By providing pre-made components, design systems reduce the time spent on repetitive tasks, allowing teams to focus on more complex design challenges."}),`
+`]}),`
+`,e.jsxs(n.li,{children:[`
+`,e.jsx(n.p,{children:"Consistency Across Products: A design system ensures that all products share a unified look and feel, which enhances brand recognition and user experience. This is crucial for companies with multiple products or teams working in silos."}),`
+`]}),`
+`,e.jsxs(n.li,{children:[`
+`,e.jsx(n.p,{children:"Improved Collaboration: With a shared language and framework, design systems facilitate better communication among designers, developers, and other stakeholders, reducing misunderstandings and misalignments."}),`
+`]}),`
+`,e.jsxs(n.li,{children:[`
+`,e.jsx(n.p,{children:"Scalability: As products evolve, design systems allow for easy updates and scalability. Changes made to components can be reflected across all products, ensuring consistency."}),`
+`]}),`
+`,e.jsxs(n.li,{children:[`
+`,e.jsx(n.p,{children:"Quality Control: A design system simplifies the review process by providing clear guidelines and standards, making it easier to identify inconsistencies and maintain high-quality design."}),`
+`]}),`
+`]}),`
+`,e.jsx(n.h1,{id:"links",children:"Links"}),`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:e.jsx(n.a,{href:"https://najwer23.github.io/najwer23snacks",rel:"nofollow",children:"Web"})}),`
+`,e.jsx(n.li,{children:e.jsx(n.a,{href:"https://github.com/najwer23/najwer23snacks",rel:"nofollow",children:"Github"})}),`
+`,e.jsx(n.li,{children:e.jsx(n.a,{href:"https://www.npmjs.com/search?q=najwer23snacks",rel:"nofollow",children:"NPM"})}),`
+`]}),`
+`,e.jsx(n.h1,{id:"lets-connect",children:"Let's connect!"}),`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:e.jsx(n.a,{href:"https://najwer23.github.io/",rel:"nofollow",children:"Portfolio"})}),`
+`,e.jsx(n.li,{children:e.jsx(n.a,{href:"https://github.com/najwer23",rel:"nofollow",children:"Github"})}),`
+`,e.jsx(n.li,{children:e.jsx(n.a,{href:"https://www.linkedin.com/in/najwer23/",rel:"nofollow",children:"Linkedin"})}),`
+`]}),`
+`,e.jsx(n.h1,{id:"author",children:"Author"}),`
+`,e.jsx(n.p,{children:"Mariusz Najwer"}),`
+`,e.jsx(n.h2,{id:"license",children:"License"}),`
+`,e.jsx(n.p,{children:e.jsx(n.a,{href:"https://opensource.org/licenses/MIT",rel:"nofollow",children:e.jsx(n.img,{src:"https://img.shields.io/badge/License-MIT-yellow.svg",alt:"License: MIT"})})})]})}function x(s={}){const{wrapper:n}={...r(),...s.components};return n?e.jsx(n,{...s,children:e.jsx(i,{...s})}):i(s)}export{x as default};
