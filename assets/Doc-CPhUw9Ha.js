8cc5a7dfbd89ff2313be8ee912efac0679ba6742
Mariusz Najwer <najwer23@gmail.com>
2024-08-24 17:34:31 +0200

    Updates

---
+++ assets/Doc-CPhUw9Ha.js
@@ -0,0 +1,66 @@
+import{j as n}from"./jsx-runtime-DEdD30eg.js";import{useMDXComponents as o}from"./index-CcnH5Kt0.js";import{ae as t}from"./index-ByB_y7bV.js";import"./index-RYns6xqu.js";import"./iframe-eRGyBmaG.js";import"../sb-preview/runtime.js";import"./index-D16Yfzz8.js";import"./index-D-8MO0q_.js";import"./index-B23dhaOI.js";import"./index-DrFu-skq.js";function i(s){const e={a:"a",blockquote:"blockquote",code:"code",h1:"h1",h2:"h2",h3:"h3",img:"img",li:"li",ol:"ol",p:"p",pre:"pre",ul:"ul",...o(),...s.components};return n.jsxs(n.Fragment,{children:[n.jsx(t,{title:"Doc"}),`
+`,n.jsx(e.h1,{id:"najwer23snacks---design-system",children:"@najwer23snacks - Design System"}),`
+`,n.jsx(e.h3,{id:"a-bundle-of-react-components-to-use-in-your-application",children:"A bundle of React components to use in your application!"}),`
+`,n.jsx(e.p,{children:'A design system is a comprehensive collection of reusable components, guidelines, and standards that help teams create consistent and cohesive digital products. It serves as a "single source of truth," ensuring that design and development processes are streamlined across various platforms and teams.'}),`
+`,n.jsxs(e.ol,{children:[`
+`,n.jsx(e.li,{children:"Reusable UI Components"}),`
+`,n.jsx(e.li,{children:"Documentation and Guidelines"}),`
+`,n.jsx(e.li,{children:"Style Guides"}),`
+`,n.jsx(e.li,{children:"Design Tokens"}),`
+`]}),`
+`,n.jsx(e.h1,{id:"benefits-of-using-a-design-system",children:"Benefits of Using a Design System"}),`
+`,n.jsxs(e.ul,{children:[`
+`,n.jsxs(e.li,{children:[`
+`,n.jsx(e.p,{children:"Increased Efficiency: By providing pre-made components, design systems reduce the time spent on repetitive tasks, allowing teams to focus on more complex design challenges."}),`
+`]}),`
+`,n.jsxs(e.li,{children:[`
+`,n.jsx(e.p,{children:"Consistency Across Products: A design system ensures that all products share a unified look and feel, which enhances brand recognition and user experience. This is crucial for companies with multiple products or teams working in silos."}),`
+`]}),`
+`,n.jsxs(e.li,{children:[`
+`,n.jsx(e.p,{children:"Improved Collaboration: With a shared language and framework, design systems facilitate better communication among designers, developers, and other stakeholders, reducing misunderstandings and misalignments."}),`
+`]}),`
+`,n.jsxs(e.li,{children:[`
+`,n.jsx(e.p,{children:"Scalability: As products evolve, design systems allow for easy updates and scalability. Changes made to components can be reflected across all products, ensuring consistency."}),`
+`]}),`
+`,n.jsxs(e.li,{children:[`
+`,n.jsx(e.p,{children:"Quality Control: A design system simplifies the review process by providing clear guidelines and standards, making it easier to identify inconsistencies and maintain high-quality design."}),`
+`]}),`
+`]}),`
+`,n.jsx(e.h1,{id:"css-tokens",children:"CSS tokens"}),`
+`,n.jsxs(e.blockquote,{children:[`
+`,n.jsx(e.p,{children:"Colors are the smiles of nature."}),`
+`]}),`
+`,n.jsx(e.pre,{children:n.jsx(e.code,{className:"language-css",children:`:root {
+  /* Fonts */
+  --n23snack-font-family: -apple-system, BlinkMacSystemFont, avenir next, avenir, segoe ui, helvetica neue, helvetica,
+    Cantarell, Ubuntu, roboto, noto, arial, sans-serif;
+
+  /* Button */
+  --n23snack-button-bg-color: #00a76f;
+  --n23snack-button-bg-color-hover: #007867;
+  --n23snack-button-bg-color-active: #004b50;
+  --n23snack-button-bg-color-focus: #00b8d9;
+  --n23snack-button-bg-color-disabled: #919eab;
+  --n23snack-button-font-color: #ffffff;
+  --n23snack-button-font-size: 18px;
+  --n23snack-button-font-weight: 700;
+  --n23snack-button-font-family: var(--n23snack-font-family);
+  --n23snack-button-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.24), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+}
+`})}),`
+`,n.jsx(e.h1,{id:"links",children:"Links"}),`
+`,n.jsxs(e.ul,{children:[`
+`,n.jsx(e.li,{children:n.jsx(e.a,{href:"https://najwer23.github.io/najwer23snacks",rel:"nofollow",children:"Web"})}),`
+`,n.jsx(e.li,{children:n.jsx(e.a,{href:"https://github.com/najwer23/najwer23snacks",rel:"nofollow",children:"Github"})}),`
+`,n.jsx(e.li,{children:n.jsx(e.a,{href:"https://www.npmjs.com/search?q=najwer23snacks",rel:"nofollow",children:"NPM"})}),`
+`]}),`
+`,n.jsx(e.h1,{id:"lets-connect",children:"Let's connect!"}),`
+`,n.jsxs(e.ul,{children:[`
+`,n.jsx(e.li,{children:n.jsx(e.a,{href:"https://najwer23.github.io/",rel:"nofollow",children:"Portfolio"})}),`
+`,n.jsx(e.li,{children:n.jsx(e.a,{href:"https://github.com/najwer23",rel:"nofollow",children:"Github"})}),`
+`,n.jsx(e.li,{children:n.jsx(e.a,{href:"https://www.linkedin.com/in/najwer23/",rel:"nofollow",children:"Linkedin"})}),`
+`]}),`
+`,n.jsx(e.h1,{id:"author",children:"Author"}),`
+`,n.jsx(e.p,{children:"Mariusz Najwer"}),`
+`,n.jsx(e.h2,{id:"license",children:"License"}),`
+`,n.jsx(e.p,{children:n.jsx(e.a,{href:"https://opensource.org/licenses/MIT",rel:"nofollow",children:n.jsx(e.img,{src:"https://img.shields.io/badge/License-MIT-yellow.svg",alt:"License: MIT"})})})]})}function x(s={}){const{wrapper:e}={...o(),...s.components};return e?n.jsx(e,{...s,children:n.jsx(i,{...s})}):i(s)}export{x as default};
