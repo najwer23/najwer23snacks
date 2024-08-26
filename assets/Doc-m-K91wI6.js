ae48b14b2377eb3c9f089727802c1aa3c573f651
Mariusz Najwer <najwer23@gmail.com>
2024-08-26 13:41:36 +0200

    Updates

---
+++ assets/Doc-m-K91wI6.js
@@ -0,0 +1,65 @@
+import{j as e}from"./jsx-runtime-DEdD30eg.js";import{useMDXComponents as t}from"./index-CcnH5Kt0.js";import{ae as i}from"./index-CYGxtR9w.js";import"./index-RYns6xqu.js";import"./iframe-CbtI-za8.js";import"../sb-preview/runtime.js";import"./index-D16Yfzz8.js";import"./index-D-8MO0q_.js";import"./index-B23dhaOI.js";import"./index-DrFu-skq.js";function o(s){const n={a:"a",blockquote:"blockquote",code:"code",h1:"h1",img:"img",li:"li",ol:"ol",p:"p",pre:"pre",ul:"ul",...t(),...s.components};return e.jsxs(e.Fragment,{children:[e.jsx(i,{title:"Doc"}),`
+`,e.jsx(n.h1,{id:"najwer23snacks",children:"@najwer23snacks"}),`
+`,e.jsxs(n.p,{children:[e.jsx(n.a,{href:"https://najwer23.github.io/najwer23snacks",rel:"nofollow",children:e.jsx(n.img,{src:"https://img.shields.io/badge/-Storybook-FF4785?style=for-the-badge&logo=storybook&logoColor=white",alt:"Storybook"})})," ",e.jsx(n.a,{href:"https://github.com/najwer23/najwer23snacks",rel:"nofollow",children:e.jsx(n.img,{src:"https://img.shields.io/badge/github%20pages-121013?style=for-the-badge&logo=github&logoColor=white",alt:"Github "})})," ",e.jsx(n.a,{href:"https://www.npmjs.com/search?q=najwer23snacks",rel:"nofollow",children:e.jsx(n.img,{src:"https://img.shields.io/badge/NPM-%23CB3837.svg?style=for-the-badge&logo=npm&logoColor=white",alt:"NPM"})})]}),`
+`,e.jsx(n.p,{children:'A design system is a comprehensive collection of reusable components, guidelines, and standards that help teams create consistent and cohesive digital products. It serves as a "single source of truth," ensuring that design and development processes are streamlined across various platforms and teams.'}),`
+`,e.jsxs(n.ol,{children:[`
+`,e.jsx(n.li,{children:"Reusable UI Components"}),`
+`,e.jsx(n.li,{children:"Documentation and Guidelines"}),`
+`,e.jsx(n.li,{children:"Style Guides"}),`
+`,e.jsx(n.li,{children:"Design Tokens"}),`
+`]}),`
+`,e.jsx(n.h1,{id:"benefits-of-using-a-design-system",children:"Benefits of Using a Design System"}),`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsxs(n.li,{children:[`
+`,e.jsx(n.p,{children:"Increased Efficiency: By providing pre-made components, design systems reduce the time spent on repetitive tasks, allowing teams to focus on more complex design challenges."}),`
+`]}),`
+`,e.jsxs(n.li,{children:[`
+`,e.jsx(n.p,{children:"Consistency Across Products: A design system ensures that all products share a unified look and feel, which enhances brand recognition and user experience. This is crucial for companies with multiple products or teams working in silos."}),`
+`]}),`
+`,e.jsxs(n.li,{children:[`
+`,e.jsx(n.p,{children:"Improved Collaboration: With a shared language and framework, design systems facilitate better communication among designers, developers, and other stakeholders, reducing misunderstandings and misalignments."}),`
+`]}),`
+`,e.jsxs(n.li,{children:[`
+`,e.jsx(n.p,{children:"Scalability: As products evolve, design systems allow for easy updates and scalability. Changes made to components can be reflected across all products, ensuring consistency."}),`
+`]}),`
+`,e.jsxs(n.li,{children:[`
+`,e.jsx(n.p,{children:"Quality Control: A design system simplifies the review process by providing clear guidelines and standards, making it easier to identify inconsistencies and maintain high-quality design."}),`
+`]}),`
+`]}),`
+`,e.jsx(n.h1,{id:"css-tokens",children:"CSS tokens"}),`
+`,e.jsxs(n.blockquote,{children:[`
+`,e.jsx(n.p,{children:"Colors are the smiles of nature."}),`
+`]}),`
+`,e.jsx(n.pre,{children:e.jsx(n.code,{className:"language-css",children:`:root {
+  /* Fonts */
+  --n23snack-font-family: -apple-system, BlinkMacSystemFont, avenir next, avenir, segoe ui, helvetica neue, helvetica,
+    Cantarell, Ubuntu, roboto, noto, arial, sans-serif;
+
+  /* Button */
+  --n23snack-button-bg-color: #00a76f;
+  --n23snack-button-bg-color-hover: #007867;
+  --n23snack-button-bg-color-active: #004b50;
+  --n23snack-button-bg-color-focus: #00b8d9;
+  --n23snack-button-bg-color-disabled: #919eab;
+  --n23snack-button-font-color: #ffffff;
+  --n23snack-button-font-size: 18px;
+  --n23snack-button-font-weight: 700;
+  --n23snack-button-font-family: var(--n23snack-font-family);
+  --n23snack-button-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.24), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+
+  /* TextBox */
+  --n23snack-textbox-h1-font-color: black;
+  --n23snack-textbox-h2-font-color: black;
+  --n23snack-textbox-h3-font-color: black;
+  --n23snack-textbox-p-font-color: black;
+  --n23snack-textbox-a-font-color: orange;
+  --n23snack-textbox-a-font-color-hover: orangered;
+}
+`})}),`
+`,e.jsx(n.h1,{id:"lets-connect",children:"Let's connect!"}),`
+`,e.jsxs(n.p,{children:[e.jsx(n.a,{href:"https://najwer23.github.io/",rel:"nofollow",children:e.jsx(n.img,{src:"https://img.shields.io/badge/-Portfolio-%239F2B68?style=flat&logo=GoogleChrome&logoColor=white",alt:"dev.to badge"})}),`
+`,e.jsx(n.a,{href:"https://najwer23.github.io/resume/",rel:"nofollow",children:e.jsx(n.img,{src:"https://img.shields.io/badge/-R%C3%A9sum%C3%A9-%23017745?style=flat&logo=AdobeAcrobatReader&logoColor=white",alt:"dev.to badge"})}),`
+`,e.jsx(n.a,{href:"https://twitter.com/najwer23",rel:"nofollow",children:e.jsx(n.img,{src:"https://img.shields.io/badge/-najwer23-%231FA1F1?style=flat&logo=x&logoColor=white",alt:"twitter badge"})}),`
+`,e.jsx(n.a,{href:"https://www.linkedin.com/in/najwer23",rel:"nofollow",children:e.jsx(n.img,{src:"https://img.shields.io/badge/-najwer23-%230177B5?style=flat&logo=linkedin",alt:"dev.to badge"})}),`
+`,e.jsx(n.a,{href:"https://leetcode.com/najwer23/",rel:"nofollow",children:e.jsx(n.img,{src:"https://img.shields.io/badge/-najwer23-%23ffffff?style=flat&logo=Leetcode",alt:"dev.to badge"})}),`
+`,e.jsx(n.a,{href:"https://www.npmjs.com/~najwer23",rel:"nofollow",children:e.jsx(n.img,{src:"https://img.shields.io/badge/-najwer23-%23cb3837?style=flat&logo=Npm",alt:"dev.to badge"})})]})]})}function b(s={}){const{wrapper:n}={...t(),...s.components};return n?e.jsx(n,{...s,children:e.jsx(o,{...s})}):o(s)}export{b as default};
