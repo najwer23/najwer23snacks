675976667fea56ca1ad04a4fe94cc06e5a4c3cbc
Mariusz Najwer <najwer23@gmail.com>
2024-08-30 09:14:49 +0200

    Plop

--- plopfile.mjs
+++ plopfile.mjs
@@ -1,45 +1,38 @@
 // https://medium.com/@aldinofrizal/file-creation-automation-with-plop-b18d09d77360
 
 export default function (plop) {
+  plop.setGenerator('Component', {
+    description: 'plop generating Component..',
 
-	plop.setGenerator("controller", {
-		description: "plop generating controller..",
+    prompts: [
+      {
+        type: 'input',
+        name: 'name',
+        message: 'Component:',
+      },
+    ],
 
-		prompts: [
-			{
-				type: "input",
-				name: "name",
-				message: "Component:",
-			},
-		],
-
-		actions: [
-			{
-				type: "add",
-				path: "src/stories/{{lowerCase name}}/{{name}}.stories.tsx",
-				templateFile: "templates/Story.txt",
-			},
-			{
-				type: "add",
-				path: "src/stories/{{lowerCase name}}/{{name}}.tsx",
-				templateFile: "templates/Main.txt",
-			},
-			{
-				type: "add",
-				path: "src/stories/{{lowerCase name}}/{{name}}.module.css",
-				templateFile: "templates/CSS.txt",
-			},
-			{
-				type: "add",
-				path: "src/stories/{{lowerCase name}}/index.ts",
-				templateFile: "templates/Index.txt",
-			},
-			{
-				type: "add",
-				path: "src/stories/{{lowerCase name}}/{{name}}.types.ts",
-				templateFile: "templates/Types.txt",
-			},
-		],
-	});
-
+    actions: [
+      {
+        type: 'add',
+        path: 'src/stories/{{lowerCase name}}/{{name}}.stories.tsx',
+        templateFile: 'templates/Story.txt',
+      },
+      {
+        type: 'add',
+        path: 'src/stories/{{lowerCase name}}/{{name}}.tsx',
+        templateFile: 'templates/Component.txt',
+      },
+      {
+        type: 'add',
+        path: 'src/stories/{{lowerCase name}}/{{name}}.module.css',
+        templateFile: 'templates/CSS.txt',
+      },
+      {
+        type: 'add',
+        path: 'src/stories/{{lowerCase name}}/index.ts',
+        templateFile: 'templates/Index.txt',
+      },
+    ],
+  });
 }
