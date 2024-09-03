90644d9eba44f7aa8fff93761f450a2975991f90
Mariusz Najwer <najwer23@gmail.com>
2024-09-03 17:42:13 +0200

    Updates

--- assets/Footer.stories-CSvKUnFh.js
+++
@@ -1,6 +0,0 @@
-import{j as o}from"./jsx-runtime-DEdD30eg.js";import{T as d}from"./TextBox-DutnzQF-.js";import{G as m}from"./Grid-DrVcZ8eD.js";import"./index-RYns6xqu.js";const x="_footerPush_1ts8l_11",f="_footerContainer_1ts8l_15",p="_footerContent_1ts8l_21",e={footerPush:x,footerContainer:f,footerContent:p},r=({footerWidthMax:i,children:c})=>o.jsxs("footer",{children:[o.jsx("div",{className:e.footerPush}),o.jsx("div",{className:e.footerContainer,children:o.jsx(m,{layout:"container",widthMax:i,padding:"0 10px 0 10px",children:o.jsx("div",{className:e.footerContent,children:c})})})]});r.displayName="Footer";r.__docgenInfo={description:"",methods:[],displayName:"Footer"};const j={title:"Footer/Footer",component:r,tags:["autodocs"]},t={args:{footerWidthMax:1e3,children:o.jsx(d,{tag:"p",children:" Mariusz Najwer, 2024 "})}};var s,n,a;t.parameters={...t.parameters,docs:{...(s=t.parameters)==null?void 0:s.docs,source:{originalSource:`{
-  args: {
-    footerWidthMax: 1000,
-    children: <TextBox tag="p"> Mariusz Najwer, 2024 </TextBox>
-  }
-}`,...(a=(n=t.parameters)==null?void 0:n.docs)==null?void 0:a.source}}};const g=["Footer"];export{t as Footer,g as __namedExportsOrder,j as default};
