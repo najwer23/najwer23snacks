10640c929b0397fca9fcdaba406e16389cd73635
Mariusz Najwer <najwer23@gmail.com>
2024-09-05 22:45:45 +0200

    Updates

--- assets/Doc-lG0SJk3J.js
+++ assets/Doc-lG0SJk3J.js
@@ -1,4 +1,4 @@
-import{j as n}from"./jsx-runtime-DEdD30eg.js";import{useMDXComponents as t}from"./index-CcnH5Kt0.js";import{ae as i}from"./index-BzVKnzyb.js";import"./index-RYns6xqu.js";import"./iframe-BJRpg6n5.js";import"../sb-preview/runtime.js";import"./index-D16Yfzz8.js";import"./index-D-8MO0q_.js";import"./index-B23dhaOI.js";import"./index-DrFu-skq.js";function o(s){const e={a:"a",blockquote:"blockquote",code:"code",h1:"h1",img:"img",li:"li",ol:"ol",p:"p",pre:"pre",ul:"ul",...t(),...s.components};return n.jsxs(n.Fragment,{children:[n.jsx(i,{title:"Doc"}),`
+import{j as n}from"./jsx-runtime-DEdD30eg.js";import{useMDXComponents as t}from"./index-CcnH5Kt0.js";import{ae as a}from"./index-0QmREs2d.js";import"./index-RYns6xqu.js";import"./iframe-B-1TCySJ.js";import"../sb-preview/runtime.js";import"./index-D16Yfzz8.js";import"./index-D-8MO0q_.js";import"./index-B23dhaOI.js";import"./index-DrFu-skq.js";function o(s){const e={a:"a",blockquote:"blockquote",code:"code",h1:"h1",img:"img",li:"li",ol:"ol",p:"p",pre:"pre",ul:"ul",...t(),...s.components};return n.jsxs(n.Fragment,{children:[n.jsx(a,{title:"Doc"}),`
 `,n.jsx(e.h1,{id:"najwer23snacks",children:"najwer23snacks"}),`
 `,n.jsxs(e.p,{children:[n.jsx(e.a,{href:"https://najwer23.github.io/najwer23snacks",rel:"nofollow",children:n.jsx(e.img,{src:"https://img.shields.io/badge/-Storybook-FF4785?style=for-the-badge&logo=storybook&logoColor=white",alt:"Storybook"})})," ",n.jsx(e.a,{href:"https://github.com/najwer23/najwer23snacks",rel:"nofollow",children:n.jsx(e.img,{src:"https://img.shields.io/badge/github-%23121011.svg?style=for-the-badge&logo=github&logoColor=white",alt:"GitHub"})})," ",n.jsx(e.a,{href:"https://www.npmjs.com/search?q=najwer23snacks",rel:"nofollow",children:n.jsx(e.img,{src:"https://img.shields.io/badge/NPM-%23CB3837.svg?style=for-the-badge&logo=npm&logoColor=white",alt:"NPM"})})]}),`
 `,n.jsx(e.p,{children:'A design system is a comprehensive collection of reusable components, guidelines, and standards that help teams create consistent and cohesive digital products. It serves as a "single source of truth," ensuring that design and development processes are streamlined across various platforms and teams.'}),`
@@ -53,51 +53,51 @@ export const Example = () => {
 `]}),`
 `,n.jsx(e.pre,{children:n.jsx(e.code,{className:"language-css",children:`:root {
   /* Fonts */
-  --n23snacks-font-family: -apple-system, BlinkMacSystemFont, avenir next, avenir, segoe ui, helvetica neue, helvetica,
+  --najwer23snacks--font-family: -apple-system, BlinkMacSystemFont, avenir next, avenir, segoe ui, helvetica neue, helvetica,
     Cantarell, Ubuntu, roboto, noto, arial, sans-serif;
 
   /* Button */
-  --n23snacks-button-bg-color: #00a76f;
-  --n23snacks-button-bg-color-hover: #007867;
-  --n23snacks-button-bg-color-active: #004b50;
-  --n23snacks-button-bg-color-focus: #00b8d9;
-  --n23snacks-button-bg-color-disabled: #919eab;
-  --n23snacks-button-font-color: #ffffff;
-  --n23snacks-button-font-size: 18px;
-  --n23snacks-button-font-weight: 700;
-  --n23snacks-button-font-family: var(--n23snacks-font-family);
-  --n23snacks-button-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.24), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+  --najwer23snacks--button-bg-color: #00a76f;
+  --najwer23snacks--button-bg-color-hover: #007867;
+  --najwer23snacks--button-bg-color-active: #004b50;
+  --najwer23snacks--button-bg-color-focus: #00b8d9;
+  --najwer23snacks--button-bg-color-disabled: #919eab;
+  --najwer23snacks--button-font-color: #ffffff;
+  --najwer23snacks--button-font-size: 18px;
+  --najwer23snacks--button-font-weight: 700;
+  --najwer23snacks--button-font-family: var(--najwer23snacks--font-family);
+  --najwer23snacks--button-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.24), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
 
   /* Navigation */
   --najwer23snacks-navigation-mobile-bg-color: white;
 
   /* TextBox */
-  --n23snacks-textbox-h1-font-color: black;
-  --n23snacks-textbox-h2-font-color: black;
-  --n23snacks-textbox-h3-font-color: black;
-  --n23snacks-textbox-p-font-color: black;
-  --n23snacks-textbox-a-font-color: orange;
-  --n23snacks-textbox-a-font-color-hover: orangered;
-  --n23snacks-textbox-title-a-font-color: green;
-  --n23snacks-textbox-title-a-font-color-hover: yellow;
+  --najwer23snacks--textbox-h1-font-color: black;
+  --najwer23snacks--textbox-h2-font-color: black;
+  --najwer23snacks--textbox-h3-font-color: black;
+  --najwer23snacks--textbox-p-font-color: black;
+  --najwer23snacks--textbox-a-font-color: orange;
+  --najwer23snacks--textbox-a-font-color-hover: orangered;
+  --najwer23snacks--textbox-title-a-font-color: green;
+  --najwer23snacks--textbox-title-a-font-color-hover: yellow;
 
   /* Input */
-  --n23snacks-input-label-font-size: 16px;
-  --n23snacks-input-label-font-family: var(--n23snacks-font-family);
-  --n23snacks-input-label-font-weight: 700;
-  --n23snacks-input-label-font-color: #161925;
+  --najwer23snacks--input-label-font-size: 16px;
+  --najwer23snacks--input-label-font-family: var(--najwer23snacks--font-family);
+  --najwer23snacks--input-label-font-weight: 700;
+  --najwer23snacks--input-label-font-color: #161925;
 
-  --n23snacks-input-input-font-size: 18px;
-  --n23snacks-input-input-font-family: var(--n23snacks-font-family);
-  --n23snacks-input-input-font-weight: 600;
-  --n23snacks-input-input-font-color: #161925;
-  --n23snacks-input-input-border-color: #161925;
-  --n23snacks-input-input-placeholder-font-weight: auto;
+  --najwer23snacks--input-input-font-size: 18px;
+  --najwer23snacks--input-input-font-family: var(--najwer23snacks--font-family);
+  --najwer23snacks--input-input-font-weight: 600;
+  --najwer23snacks--input-input-font-color: #161925;
+  --najwer23snacks--input-input-border-color: #161925;
+  --najwer23snacks--input-input-placeholder-font-weight: auto;
 
-  --n23snacks-input-error-font-size: 14px;
-  --n23snacks-input-error-font-family: var(--n23snacks-font-family);
-  --n23snacks-input-error-font-weight: 600;
-  --n23snacks-input-error-font-color: #ff3333;
+  --najwer23snacks--input-error-font-size: 14px;
+  --najwer23snacks--input-error-font-family: var(--najwer23snacks--font-family);
+  --najwer23snacks--input-error-font-weight: 600;
+  --najwer23snacks--input-error-font-color: #ff3333;
 }
 `})}),`
 `,n.jsx(e.h1,{id:"lets-connect",children:"Let's connect!"}),`
@@ -106,4 +106,4 @@ export const Example = () => {
 `,n.jsx(e.a,{href:"https://twitter.com/najwer23",rel:"nofollow",children:n.jsx(e.img,{src:"https://img.shields.io/badge/-najwer23-%231FA1F1?style=flat&logo=x&logoColor=white",alt:"twitter badge"})}),`
 `,n.jsx(e.a,{href:"https://www.linkedin.com/in/najwer23",rel:"nofollow",children:n.jsx(e.img,{src:"https://img.shields.io/badge/-najwer23-%230177B5?style=flat&logo=linkedin",alt:"dev.to badge"})}),`
 `,n.jsx(e.a,{href:"https://leetcode.com/najwer23/",rel:"nofollow",children:n.jsx(e.img,{src:"https://img.shields.io/badge/-najwer23-%23ffffff?style=flat&logo=Leetcode",alt:"dev.to badge"})}),`
-`,n.jsx(e.a,{href:"https://www.npmjs.com/~najwer23",rel:"nofollow",children:n.jsx(e.img,{src:"https://img.shields.io/badge/-najwer23-%23cb3837?style=flat&logo=Npm",alt:"dev.to badge"})})]})]})}function u(s={}){const{wrapper:e}={...t(),...s.components};return e?n.jsx(e,{...s,children:n.jsx(o,{...s})}):o(s)}export{u as default};
+`,n.jsx(e.a,{href:"https://www.npmjs.com/~najwer23",rel:"nofollow",children:n.jsx(e.img,{src:"https://img.shields.io/badge/-najwer23-%23cb3837?style=flat&logo=Npm",alt:"dev.to badge"})})]})]})}function f(s={}){const{wrapper:e}={...t(),...s.components};return e?n.jsx(e,{...s,children:n.jsx(o,{...s})}):o(s)}export{f as default};
