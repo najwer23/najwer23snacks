c702d7eb9686c99d061b993377d0638ea9689e5c
Mariusz Najwer <najwer23@gmail.com>
2024-09-10 15:20:20 +0200

    Updates

---
+++ assets/Select.stories-CITjPAj4.js
@@ -0,0 +1,13 @@
+import{j as t}from"./jsx-runtime-DEdD30eg.js";import{S as s}from"./Select-BWEMnlLl.js";import"./index-RYns6xqu.js";import"./Input-BGhH-L1E.js";const i={title:"Inputs/Select",component:s,parameters:{layout:"centered"},tags:["autodocs"]},e={args:{name:"mylabel",label:"Label",validatorOptions:[{type:"empty"}],selectOptions:["Option1","Option2"]},decorators:[n=>t.jsx(t.Fragment,{children:t.jsx(n,{})})]};var o,r,a;e.parameters={...e.parameters,docs:{...(o=e.parameters)==null?void 0:o.docs,source:{originalSource:`{
+  args: {
+    name: 'mylabel',
+    label: 'Label',
+    validatorOptions: [{
+      type: 'empty'
+    }],
+    selectOptions: ['Option1', 'Option2']
+  },
+  decorators: [Story => <>
+        <Story />
+      </>]
+}`,...(a=(r=e.parameters)==null?void 0:r.docs)==null?void 0:a.source}}};const d=["Select"];export{e as Select,d as __namedExportsOrder,i as default};
