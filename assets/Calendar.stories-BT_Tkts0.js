f3cd96e61bb7b1b2232951a3d9aa6f59e96c74a3
Mariusz Najwer <najwer23@gmail.com>
2024-09-10 13:15:38 +0200

    Updates

---
+++ assets/Calendar.stories-BT_Tkts0.js
@@ -0,0 +1,14 @@
+import{j as t}from"./jsx-runtime-DEdD30eg.js";import{C as s}from"./Calendar-DEAE1dJL.js";import"./index-RYns6xqu.js";import"./Input-BGhH-L1E.js";import"./Button-B_v8z9C9.js";const i={title:"Inputs/Calendar",component:s,parameters:{layout:"centered"},tags:["autodocs"]},e={args:{name:"date",label:"Date",validatorOptions:[{type:"empty"},{type:"date"}]},decorators:[n=>t.jsx(t.Fragment,{children:t.jsx(n,{})})]};var a,r,o;e.parameters={...e.parameters,docs:{...(a=e.parameters)==null?void 0:a.docs,source:{originalSource:`{
+  args: {
+    name: 'date',
+    label: 'Date',
+    validatorOptions: [{
+      type: 'empty'
+    }, {
+      type: 'date'
+    }]
+  },
+  decorators: [Story => <>
+        <Story />
+      </>]
+}`,...(o=(r=e.parameters)==null?void 0:r.docs)==null?void 0:o.source}}};const y=["Calendar"];export{e as Calendar,y as __namedExportsOrder,i as default};
