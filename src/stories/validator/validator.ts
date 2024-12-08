59c5e270230178450337776a34282c0d063f15d5
Mariusz Najwer <najwer23@gmail.com>
2024-12-08 09:49:22 +0100

    Error in Date validation

--- src/stories/validator/validator.ts
+++ src/stories/validator/validator.ts
@@ -73,7 +73,7 @@ const testDate = (value: string): string | null => {
 
     if (year % 400 == 0 || (year % 100 != 0 && year % 4 == 0)) monthLength[1] = 29;
 
-    return day > 0 && day <= monthLength[month - 1];
+    return day > 0 && day <= monthLength[month];
   };
 
   if (!checkIfDateIsCorrect(value)) {
